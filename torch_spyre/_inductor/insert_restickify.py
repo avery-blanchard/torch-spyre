c59188ac7b9a2affbd1ba3968caca0267b691438
Avery Blanchard <avery.blanchard@ibm.com>
2026-08-07 09:23:08 -0400

    Clean up
    Signed-off-by: Avery Blanchard <avery.blanchard@ibm.com>

--- torch_spyre/_inductor/insert_restickify.py
+++ torch_spyre/_inductor/insert_restickify.py
@@ -48,8 +48,8 @@ logger = get_inductor_logger("insert_restickify")
 
 def _fixed_tiled(layout: FixedLayout, stl: SpyreTensorLayout) -> FixedTiledLayout:
     return FixedTiledLayout(
-        layout.device, layout.dtype, layout.size, layout.stride, stl
-    )
+        layout.device, layout.dtype, layout.size, layout.stride, stl, layout.offset
+    )  # type: ignore[call-arg]
 
 
 def _record_restickify(
